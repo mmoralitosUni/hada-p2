c0870147d0d496a6a050a185311a5fa9dcf6ab29
mmorales99 <mjmorales.mcv@gmail.com>
2021-02-24 20:10:38 +0100

    48789509T Añade los métodos de la clase Jugador

--- hada-p2/Jugador.cs
+++ hada-p2/Jugador.cs
@@ -8,16 +8,18 @@ namespace Hada
 {
     class Jugador
     {
+        /// Campos
+
         public static int maxAmonestaciones { get; set; }
         public static int maxFaltas { get; set; }
         public static int minEnergia { get; set; }
         public static Random rand { private get; set; }
         public string nombre { get; private set; }
         public int puntos { get; set; }
-        private int _amonestaciones { set;  get; }
-        private int amonestaciones { 
-            set { amonestaciones = _amonestaciones = (value > maxAmonestaciones ? throw new amonestacionesMaximoExcedido :(value<0?0:value)); }
-            get { return _amonestaciones; } 
+        private int _amonestaciones { set; get; }
+        private int amonestaciones {
+            set { amonestaciones = _amonestaciones = (value > maxAmonestaciones ? throw new amonestacionesMaximoExcedido : (value < 0 ? 0 : value)); }
+            get { return _amonestaciones; }
         }
         private int _faltas { get; set; }
         private int faltas {
@@ -30,6 +32,54 @@ namespace Hada
             get { return _energia; }
         }
 
+        /// Metodos
 
+        public Jugador(string nombre, int amonestaciones, int faltas, int energia, int puntos)
+        {
+            this.nombre = nombre;
+            this.amonestaciones = amonestaciones;
+            this.faltas = faltas;
+            this.energia = energia;
+            this.puntos = puntos;
+        }
+        public void incAmonestaciones()
+        {
+            this.amonestaciones += rand.Next(0, 2 + 1);
+        }
+        public void incFaltas()
+        {
+            this.faltas += rand.Next(0, 3 + 1);
+        }
+        public void decEnergia()
+        {
+            this.energia -= rand.Next(1, 7 + 1);
+        }
+        public void incPuntos()
+        {
+            this.puntos += rand.Next(0, 3 + 1);
+        }
+        public bool todoOk()
+        {
+            bool ret = false;
+            if (this.amonestaciones < Jugador.maxAmonestaciones && this.energia >= Jugador.minEnergia && this.faltas <= Jugador.maxFaltas)
+                ret = true;
+            return ret;
+        }
+        public void mover()
+        {
+            if (this.todoOk() == true)
+            {
+                // TODO -> cambiar posicion del jugador en el tablero
+                this.incAmonestaciones();
+                this.incFaltas();
+                this.incPuntos();
+                this.decEnergia();
+            }
+        }
+        override public string toString()
+        {
+            return "[" + this.nombre + "] Puntos: " + this.puntos + "; Amonestaciones: " + this.amonestaciones + "; Faltas: " + this.faltas + "; Energia:" + this.energia + "%; Ok:" + this.todoOk() + "\n";
+        }
+        
     }
 }
